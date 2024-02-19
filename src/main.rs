e2314e05ddca9f70f56e031ec70d8c521e147b70
Léo LEESCO <leo.leesco@gmail.com>
2024-02-19 10:20:56 +0100

    refactored using nested matches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use rand::Rng;
+
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let n = 100;
+    println!("Guess the number between 1 and {n}!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=n);
+
+    println!("Please input your guess.");
+
+    while {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        match guess.trim().parse::<usize>() {
+            Ok(num) => {
+                println!("You guessed: {num}");
+                match num.cmp(&secret_number) {
+                    Ordering::Less => {
+                        println!("Too small! Try again!");
+                        true
+                    }
+                    Ordering::Greater => {
+                        println!("Too big! Try again!");
+                        true
+                    }
+                    Ordering::Equal => {
+                        println!("You win!");
+                        false
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Please type a number!");
+                true
+            }
+        }
+    } {} // that's a do-while loop
 }
